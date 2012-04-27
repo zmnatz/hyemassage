6e186c1a058d25e3297bb72af5d11866873653b5
Zaven Mnatzakanian <zmnatz@gmail.com>
2012-04-27 08:11:38 -0400

    added details

--- hyemassage.js
+++ hyemassage.js
@@ -1,24 +1,23 @@
 $(function() {
-	$(window).hashchange(function()
-	{
-		handleChange(location.hash);
-	});
 	var hash = location.hash;
 	if(!location.hash)
 	{
 		hash = '#home';
 	}
 	handleChange(hash);
+	
+	$("li.hideOnNoScript,a.hideOnNoScript").show();
+			
+	$(window).hashchange(function()
+	{
+		handleChange(location.hash);
+	});
 });
 
 function handleChange(hash)
 {
-	$('content div.section').each(function(i,value){
-		$(value).hide();
-	});
-	$('header ul li').each(function(i,value){
-		$(value).removeClass('selected')	
-	});
+	$('content div.section').hide();
+	$('header ul li').removeClass('selected');
 	$(hash).fadeIn();
 	$(hash+'-link').addClass('selected');
 }
