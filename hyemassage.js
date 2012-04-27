958497849965488a676133cb20d6bc9020082911
Zaven Mnatzakanian <zmnatz@gmail.com>
2012-04-27 14:02:08 -0400

    Added contact page

--- hyemassage.js
+++ hyemassage.js
@@ -11,13 +11,16 @@ $(function() {
 			
 	$(window).hashchange(function()
 	{
-		handleChange(location.hash);
+        if(location.hash=='#contact')
+            window.location.reload();
+        else
+		    handleChange(location.hash);
 	});
 });
 
 function handleChange(hash)
 {
-	$('content div.section').hide();
+    $('content div.section').hide();
 	$('header ul li').removeClass('selected');
 	$(hash).fadeIn();
 	$(hash+'-link').addClass('selected');
