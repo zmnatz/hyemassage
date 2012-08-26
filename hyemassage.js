ca2f6737e727dbec4f2dcedc58fcd1e04787a1df
Zaven Mnatzakanian <zmnatz@gmail.com>
2012-08-25 21:09:19 -0400

    Updated for automated loading of testimonials

--- hyemassage.js
+++ hyemassage.js
@@ -13,6 +13,8 @@ $(function() {
 	{
         handleChange(location.hash);
 	});
+	
+	loadTestimonials('json/testimonials.json','#testimonials');
 });
 
 function handleChange(hash)
@@ -21,4 +23,15 @@ function handleChange(hash)
 	$('header ul li').removeClass('selected');
 	$(hash).fadeIn();
 	$(hash+'-link').addClass('selected');
+}
+
+function loadTestimonials(url,outputDiv)
+{
+	$.getJSON(url, function(data) {url
+    	$.each(data.testimonials, function(index,testimonial) {
+    		var output = '<p>"'+testimonial.text + '"<br/>';
+    		output += ' - ' + testimonial.reviewer + '</p>';
+    		$(outputDiv).append(output);
+    	})
+	});
 }
