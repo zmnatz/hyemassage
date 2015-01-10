6bfb9125b796d1bff9d06a97f7dd17cf3bd94468
Zaven Mnatzakanian <zmnatz@Zavens-MacBook-Pro-4.local>
2015-01-10 13:27:02 -0500

    jsLint compliant

--- hyemassage.js
+++ hyemassage.js
@@ -1,32 +1,26 @@
-$(function() {
-	$(".hideOnNoScript").show();
-	$(window).hashchange(handleHashChange);		
-	if(!location.hash)
-		location.hash = '#home';
-	else
-		handleHashChange();
+/*globals $ */
+$(function () {
+    'use strict';
+    
+    function handleHashChange() {
+        var hash = location.hash || '#home';
+        $('.section').hide();
+        $('.nav-item').removeClass('selected');
+        $(hash).fadeIn();
+        $(hash + '-link').addClass('selected');
+    }
+    
+    function loadTestimonials(url, outputDiv) {
+        $.getJSON(url, function (data) {
+            $.each(data.testimonials, function (index, testimonial) {
+                $(outputDiv).append($('<p/>').html('"' + testimonial.text + '"')
+                    .append('<br/>').append(' - ' + testimonial.reviewer));
+            });
+        });
+    }
+    
+    $(".hideOnNoScript").show();
+	$(window).hashchange(handleHashChange);
+    handleHashChange();
+    loadTestimonials('json/testimonials.json', '#testimonials');
 });
-
-$(function(){
-	loadTestimonials('json/testimonials.json','#testimonials');
-});
-
-function handleHashChange()
-{
-	var hash = location.hash ? location.hash : '#home';
-    $('.section').hide();
-	$('.nav-item').removeClass('selected');
-	$(hash).fadeIn();
-	$(hash+'-link').addClass('selected');
-}
-
-function loadTestimonials(url,outputDiv)
-{
-	$.getJSON(url, function(data) {url
-    	$.each(data.testimonials, function(index,testimonial) {
-    		var output = '<p>"'+testimonial.text + '"<br/>';
-    		output += ' - ' + testimonial.reviewer + '</p>';
-    		$(outputDiv).append(output);
-    	})
-	});
-}
